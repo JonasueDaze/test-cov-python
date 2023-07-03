1a6bd8a10e873b9d919b918615deff7697c0a685
Jonathan Takeshi Ywahashi <jonathan.ywahashi@gmail.com>
2023-07-03 19:54:01 -0300

    test: fixes test coverage report

--- conftest.py
+++ conftest.py
@@ -1,3 +1,4 @@
+from threading import Thread
 import pytest
 from app import create_app
 
@@ -5,4 +6,22 @@ from app import create_app
 @pytest.fixture(scope="session")
 def app():
     app = create_app()
-    return app
+    app.config["TESTING"] = True
+    yield app
+
+
+@pytest.fixture(scope="session")
+def server(app):
+    def start_flask_server():
+        app.run()
+
+    # Start the Flask server in a separate thread
+    thread = Thread(target=start_flask_server)
+    thread.daemon = True
+    thread.start()
+
+    # Wait for the server to start
+    # Add any necessary additional wait time or checks here
+    # For simplicity, we'll use a static wait time in this example
+    import time
+    time.sleep(2)
--- test/test_unittest.py
+++ test/test_unittest.py
@@ -1,26 +1,37 @@
+from threading import Thread
 import unittest
-from flask_testing import LiveServerTestCase
 
 from app import create_app
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 
-class TestPeopleCrud(LiveServerTestCase):
-    def create_app(self):
-        app = create_app()
-        app.config["TESTING"] = True
-        return app
-
+class TestPeopleCrud(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Chrome()
+
+        app = create_app()
+        app.config["TESTING"] = True
+
+        def start_flask_server():
+            app.run()
+
+        # Start the Flask server in a separate thread
+        thread = Thread(target=start_flask_server)
+        thread.daemon = True
+        thread.start()
+
+        # Wait for the server to start
+        # Add any necessary additional wait time or checks here
+        # For simplicity, we'll use a static wait time in this example
+        import time
+        time.sleep(2)
 
     def tearDown(self):
         self.driver.quit()
 
     def test_people_crud(self):
-        # url = "http://localhost:5000/"
-        url = self.get_server_url()
+        url = "http://localhost:5000/"
         self.driver.get(url)
         self.driver.set_window_size(1050, 708)
         self.driver.find_element(By.LINK_TEXT, "+ Add").click()
@@ -33,7 +44,3 @@ class TestPeopleCrud(LiveServerTestCase):
         self.driver.find_element(By.ID, "personAge").send_keys("30")
         self.driver.find_element(By.ID, "submitBtn").click()
         self.driver.find_element(By.CSS_SELECTOR, ".btn-danger").click()
-
-
-if __name__ == "__main__":
-    unittest.main()
